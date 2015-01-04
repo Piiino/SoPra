baef7c83f8117646b5b80cc8ed2493539ee832ec
xShenx <adrian.weller94@gmail.com>
2015-01-04 15:25:31 +0100

    + Klassenrohlinge erstellt

---
+++ src/databaseController/TournamentData.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseController;
+
+/**
+ *
+ * @author Adrian
+ */
+public class TournamentData {
+    
+}

ce05332c58188f7268ddd82bb09f09daf47a5055
UntilUDie <saim.arslantepe@web.de>
2015-01-04 16:23:14 +0100

    PlayerData -> Voranmeldung und Spieler Hinzufügen Methoden geschrieben

--- src/databaseController/PlayerData.java
+++ src/databaseController/PlayerData.java
@@ -11,4 +11,15 @@ package databaseController;
  */
 public class PlayerData {
     
+    
+    //Mehtode zur Voranmledung
+    public void WriteVoranmeldungToDatabase(String Vorname, String Nickname, String Nachname, String Mail, Object Gender, boolean Gezahlt) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    //Methode zum Spieler Hinzufügen
+    public void WriteGamerToDatabase(String Vorname, String Nachname, String Nickname, String Mail, Object Geschlecht, boolean GoT, boolean AN, boolean LoW, boolean StarWars) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
 }
